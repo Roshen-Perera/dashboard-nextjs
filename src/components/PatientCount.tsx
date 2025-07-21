ce661d817ac44987e468ccca67d1ed3ab4283f66
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-21 15:28:05 +0530

    Add PatientCount component with card layout for displaying patient information

---
+++ src/components/PatientCount.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Card, CardHeader, CardTitle, CardDescription, CardAction, CardContent, CardFooter } from './ui/card';
+
+const PatientCount = () => {
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+          <CardAction>Card Action</CardAction>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
+
+export default PatientCount
