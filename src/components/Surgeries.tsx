61f2182148958d6bfa37a7f681b2644fa8b5e0ca
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-21 21:46:51 +0530

    Add a component to display total surgeries

---
+++ src/components/Surgeries.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Card, CardHeader, CardTitle, CardContent } from "./ui/card";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { FemaleSymbolIcon, MaleSymbolIcon, UserMultipleIcon } from "@hugeicons/core-free-icons";
+import { Hospital01Icon } from "@hugeicons/core-free-icons";
+
+interface PatientCountProps {
+  surgeries: number
+}
+
+const PatientCount: React.FC<PatientCountProps> = ({ surgeries }) => {
+  return (
+    <>
+      <Card className="flex w-[210px] h-[100px]">
+        <CardHeader className="flex -mt-2 items-center gap-2 ">
+          <CardTitle>Total Surgeries</CardTitle>
+          <HugeiconsIcon icon={Hospital01Icon} size={16} />
+        </CardHeader>
+        <CardContent>
+          <div className="-mt-2 flex flex-row justify-between items-center">
+            <div className="text-xl">{surgeries}</div>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default PatientCount;
