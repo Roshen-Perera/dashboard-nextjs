fab9e13fdef33de6f9d21e80a1b8b927db6b829d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-21 21:58:11 +0530

    Add NextPatientDetails component for displaying patient information

---
+++ src/components/NextPatientDetails.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NextPatientDetails = () => {
+  return (
+    <>
+      <div className='w-[500px]'>
+        <div className="flex flex-row justify-between">
+          <p>Next Patient Details</p>
+          <link>View Profile</link>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NextPatientDetails
