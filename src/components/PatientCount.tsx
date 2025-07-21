283a5420fdde44b6f0202abd23c761392df4b9b7
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-21 21:39:42 +0530

    Refactor PatientCount component to accept props for male and female patient counts and update layout for better readability

--- src/components/PatientCount.tsx
+++ src/components/PatientCount.tsx
@@ -7,27 +7,32 @@ import {
   UserMultipleIcon,
 } from "@hugeicons/core-free-icons";
 
-const PatientCount = () => {
+interface PatientCountProps {
+  totalMalePatients: number;
+  totalFemalePatients: number;
+}
+
+const PatientCount: React.FC<PatientCountProps> = ({ totalMalePatients, totalFemalePatients }) => {
   return (
     <>
       <Card className="flex w-[210px] h-[100px]">
         <CardHeader className="flex -mt-2 items-center gap-2 ">
-          <CardTitle>Card Title</CardTitle>
+          <CardTitle>Total Patients</CardTitle>
           <HugeiconsIcon icon={UserMultipleIcon} size={16} />
         </CardHeader>
         <CardContent>
           <div className="-mt-2 flex flex-row justify-between items-center">
-            <div className="flex">
-              <HugeiconsIcon icon={MaleSymbolIcon} size={35} color="#00beff" />
-              {85}
+            <div className="flex gap-2">
+              <HugeiconsIcon icon={MaleSymbolIcon} size={25} color="#00beff" />
+              <div className="text-xl">{totalMalePatients}</div>
             </div>
-            <div className="flex">
+            <div className="flex gap-2">
               <HugeiconsIcon
                 icon={FemaleSymbolIcon}
-                size={35}
+                size={25}
                 color="#FF00CA"
               />
-              {85}
+              <div className="text-xl">{totalFemalePatients}</div>
             </div>
           </div>
         </CardContent>
