9dd55ec62132ae2265419c7e53cccec0ed4e3059
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-21 16:52:52 +0530

    Refactor PatientCount component layout and improve icon rendering

--- src/components/PatientCount.tsx
+++ src/components/PatientCount.tsx
@@ -1,19 +1,39 @@
-import React from 'react'
-import { Card, CardHeader, CardTitle, CardContent } from './ui/card';
+import React from "react";
+import { Card, CardHeader, CardTitle, CardContent } from "./ui/card";
+import { HugeiconsIcon } from "@hugeicons/react";
+import {
+  FemaleSymbolIcon,
+  MaleSymbolIcon,
+  UserMultipleIcon,
+} from "@hugeicons/core-free-icons";
 
 const PatientCount = () => {
   return (
     <>
       <Card className="flex w-[210px] h-[100px]">
-        <CardHeader>
+        <CardHeader className="flex -mt-2 items-center gap-2 ">
           <CardTitle>Card Title</CardTitle>
+          <HugeiconsIcon icon={UserMultipleIcon} size={16} />
         </CardHeader>
         <CardContent>
-          <p>Card Content</p>
+          <div className="-mt-2 flex flex-row justify-between items-center">
+            <div className="flex">
+              <HugeiconsIcon icon={MaleSymbolIcon} size={35} color="#00beff" />
+              {85}
+            </div>
+            <div className="flex">
+              <HugeiconsIcon
+                icon={FemaleSymbolIcon}
+                size={35}
+                color="#FF00CA"
+              />
+              {85}
+            </div>
+          </div>
         </CardContent>
       </Card>
     </>
   );
-}
+};
 
-export default PatientCount
+export default PatientCount;
