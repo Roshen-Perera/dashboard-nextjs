0cd26f377f95d23f2951e397683bfd49734b509a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-22 16:49:27 +0530

    Adjusted size of Appointment card component

---
+++ src/components/NextAppointments.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card } from './ui/card';
+
+const NextAppointments = () => {
+  return (
+    <div>
+      <Card className="w-[348px] h-[292px] flex flex-col justify-between p-4"></Card>
+    </div>
+  );
+}
+
+export default NextAppointments
