9a1cac0988e95ef7f21a0a8e89d5606beb4e8b73
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-23 13:39:43 +0530

    Refactor layout to use Inter font and update TodaySurgeries component structure

--- src/components/TodaySurgeries.tsx
+++ src/components/TodaySurgeries.tsx
@@ -5,11 +5,15 @@ const TodaySurgeries = () => {
   return (
     <>
       <Card className="w-[662px] h-[252px] flex flex-col justify-between p-4">
-
+        <div className="-mt-2 flex flex-row justify-between">
+          <p className="text-xl">Today&apos;s Surgeries</p>
+          <a href="#" className="text-green-500 text-sm">
+            View All Sugeries{">"}
+          </a>
+        </div>
       </Card>
-
     </>
-  )
+  ); 
 }
 
 export default TodaySurgeries
