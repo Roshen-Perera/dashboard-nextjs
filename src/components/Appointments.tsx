7ea6b2e540e97e387c25017690074ee2a5861356
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-24 12:34:21 +0530

    Created Reusable Appointments compneny

---
+++ src/components/Appointments.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Image from 'next/image'
+
+interface AppointmentProps {
+  name: string;
+  image: string;
+  type: string;
+  time: string;
+}
+
+const Appointments: React.FC<AppointmentProps> = ({ name, image, type, time }) => {
+  return (
+    <div className="flex flex-row justify-between items-center gap-4">
+      <div className="flex items-center gap-4">
+        <Image
+          src={image}
+          alt={`Appointment for ${name}`}
+          width={50}
+          height={50}
+        className="rounded-full"
+      />
+      <div className="flex flex-col justify-center">
+        <p className="text-base font-medium">{name}</p>
+        <p className="text-sm text-muted-foreground">{type}</p>
+      </div>
+      </div>
+      <p className="text-2xl">{time}</p>
+    </div>
+  );
+}
+
+export default Appointments
--- src/components/NextAppointments.tsx
+++ src/components/NextAppointments.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import { Card } from './ui/card';
+import Appointments from './Appointments';
+import Image from 'next/image';
 
 const NextAppointments = () => {
   return (
     <div>
-      <Card className="w-[348px] h-[276px] flex flex-col justify-between p-4">
+      <Card className="w-[348px] h-[276px] flex flex-col p-4">
         <div className="-mt-2 flex flex-row justify-between">
-          <p className="text-xl">Today&apos;s Surgeries</p>
+          <p className="text-xl">Today&apos;s Appointments</p>
+        </div>
+        <div className="flex flex-col gap-6">
+          {/* Appointment 1 */}
+          <Appointments
+            name="John Doe"
+            time="10:00 AM"
+            type="Confirmed"
+            image="/assets/bronson.png"
+          />
+          <Appointments
+            name="John Doe"
+            time="10:00 AM"
+            type="Confirmed"
+            image="/assets/bronson.png"
+          />
+          
         </div>
       </Card>
     </div>
