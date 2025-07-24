5c8299ff18a936f4d5461e3055835dc7440bd07f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-24 21:19:19 +0530

    feat: update AppSidebar to include new menu items and icons, removing unused components

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,19 +1,19 @@
 import * as React from "react"
 
-import { SearchForm } from "@/components/search-form"
-import { VersionSwitcher } from "@/components/version-switcher"
 import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
   SidebarGroupContent,
-  SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarRail,
 } from "@/components/ui/sidebar"
+import { LayoutDashboard } from "lucide-react";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { Calendar03Icon, DashboardSquare03Icon, Doctor01Icon, MedicalFileIcon, Message01FreeIcons, PatientIcon, PaymentIcon, RecordIcon, UserSquareIcon } from "@hugeicons/core-free-icons";
 
 // This is sample data.
 const data = {
@@ -24,151 +24,80 @@ const data = {
       url: "#",
       items: [
         {
-          title: "Installation",
+          title: "Dashboard",
           url: "#",
+          isActive: false,
+          icon: DashboardSquare03Icon,
         },
         {
-          title: "Project Structure",
+          title: "Profile",
           url: "#",
-        },
-      ],
-    },
-    {
-      title: "Building Your Application",
-      url: "#",
-      items: [
-        {
-          title: "Routing",
-          url: "#",
+          isActive: false,
+          icon: UserSquareIcon,
         },
         {
-          title: "Data Fetching",
+          title: "Appointments",
           url: "#",
-          isActive: true,
+          isActive: false,
+          icon: Calendar03Icon,
         },
         {
-          title: "Rendering",
+          title: "Patients",
           url: "#",
+          isActive: false,
+          icon: PatientIcon,
         },
         {
-          title: "Caching",
+          title: "Surgeries",
           url: "#",
+          isActive: false,
+          icon: Doctor01Icon,
         },
         {
-          title: "Styling",
+          title: "Payments",
           url: "#",
+          isActive: false,
+          icon: PaymentIcon,
         },
         {
-          title: "Optimizing",
+          title: "Medical Records",
           url: "#",
+          isActive: false,
+          icon: MedicalFileIcon,
         },
         {
-          title: "Configuring",
+          title: "Messages",
           url: "#",
-        },
-        {
-          title: "Testing",
-          url: "#",
-        },
-        {
-          title: "Authentication",
-          url: "#",
-        },
-        {
-          title: "Deploying",
-          url: "#",
-        },
-        {
-          title: "Upgrading",
-          url: "#",
-        },
-        {
-          title: "Examples",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "API Reference",
-      url: "#",
-      items: [
-        {
-          title: "Components",
-          url: "#",
-        },
-        {
-          title: "File Conventions",
-          url: "#",
-        },
-        {
-          title: "Functions",
-          url: "#",
-        },
-        {
-          title: "next.config.js Options",
-          url: "#",
-        },
-        {
-          title: "CLI",
-          url: "#",
-        },
-        {
-          title: "Edge Runtime",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Architecture",
-      url: "#",
-      items: [
-        {
-          title: "Accessibility",
-          url: "#",
-        },
-        {
-          title: "Fast Refresh",
-          url: "#",
-        },
-        {
-          title: "Next.js Compiler",
-          url: "#",
-        },
-        {
-          title: "Supported Browsers",
-          url: "#",
-        },
-        {
-          title: "Turbopack",
-          url: "#",
+          isActive: false,
+          icon: Message01FreeIcons,
         },
       ],
     },
   ],
-}
+};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar {...props}>
       <SidebarHeader>
-        <VersionSwitcher
-          versions={data.versions}
-          defaultVersion={data.versions[0]}
-        />
-        <SearchForm />
+        <p className="text-3xl font-semibold">MediSync</p>
       </SidebarHeader>
       <SidebarContent>
         {/* We create a SidebarGroup for each parent. */}
         {data.navMain.map((item) => (
           <SidebarGroup key={item.title}>
-            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
             <SidebarGroupContent>
               <SidebarMenu>
                 {item.items.map((item) => (
                   <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild isActive={item.isActive}>
-                      <a href={item.url}>{item.title}</a>
-                    </SidebarMenuButton>
+                    <div className="flex items-center gap-2">
+                      <SidebarMenuButton asChild isActive={item.isActive}>
+                        <a href={item.url} className="flex items-center gap-2">
+                          <HugeiconsIcon icon={item.icon} />
+                          {item.title}
+                        </a>
+                      </SidebarMenuButton>
+                    </div>
                   </SidebarMenuItem>
                 ))}
               </SidebarMenu>
@@ -178,5 +107,5 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarContent>
       <SidebarRail />
     </Sidebar>
-  )
+  );
 }
