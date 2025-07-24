3f0867e0aa8f802315de532845f67a63ae95fc60
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-24 13:55:19 +0530

    Add Notice component with initial layout and styles

---
+++ src/components/Notice.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notice = () => {
+  return (
+    <div className='bg-#f5fffa w-[348px] h-[276px] flex flex-col p-4'>
+      
+    </div>
+  )
+}
+
+export default Notice
