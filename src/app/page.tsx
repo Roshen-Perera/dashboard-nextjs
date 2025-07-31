72cdc5cf7f9a107953ca2d1d05db565d80520384
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-31 09:17:20 +0530

    move dashboard component to home.tsx and update import path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { getServerSession } from "next-auth";
 // Update the import path to the correct location of authOptions
 import { authOptions } from "@/lib/auth"; // ✅ use alias or relative path
 import { redirect } from "next/navigation";
-import Home from "./dashboard/page";
+import Home from "./home";
 
 export default async function DashboardPage() {
   const session = await getServerSession(authOptions);
