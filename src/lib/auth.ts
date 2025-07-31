e6a3ecb3853f33d0d546155c0e2b415418f8f78e
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-31 09:48:13 +0530

    uncomment CredentialsProvider and bcrypt imports in auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,36 +2,36 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import { AuthOptions, SessionStrategy } from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
-// import bcrypt from "bcrypt";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
 export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
-        // CredentialsProvider({
-        //     name: "Credentials",
-        //     credentials: {
-        //         name: { label: "Username", type: "text", placeholder: "John Doe" },
-        //         password: { label: "Password", type: "password" },
-        //         email: { label: "Email", type: "email", placeholder: "johndoe@gmail.com" },
-        //     },
-        //     async authorize(credentials) {
-        //         if (!credentials?.email || !credentials?.password) return null;
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                name: { label: "Username", type: "text", placeholder: "John Doe" },
+                password: { label: "Password", type: "password" },
+                email: { label: "Email", type: "email", placeholder: "johndoe@gmail.com" },
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) return null;
 
-        //         const user = await prisma.user.findUnique({
-        //             where: { email: credentials.email },
-        //         });
+                const user = await prisma.user.findUnique({
+                    where: { email: credentials.email },
+                });
 
-        //         if (!user) throw new Error("No user found with the email");
+                if (!user) throw new Error("No user found with the email");
 
-        //         const passwordMatch = await bcrypt.compare(credentials.password, user.password || "");
-        //         if (!passwordMatch) throw new Error("Invalid password");
+                const passwordMatch = await bcrypt.compare(credentials.password, user.password || "");
+                if (!passwordMatch) throw new Error("Invalid password");
 
-        //         return user;
-        //     },
-        // }),
+                return user;
+            },
+        }),
     ],
     session: {
         strategy: "jwt" as SessionStrategy,
