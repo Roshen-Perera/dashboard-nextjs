04b91aefaa86963e9e748e856abcf089a9deb31f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-05 17:13:21 +0530

    Update padding in profile and home page layouts for consistent spacing

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -11,7 +11,7 @@ const Home = () => {
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-          <div className="pl-4 pr-6 bg-[#f5fffa]">
+          <div className="pl-4 pr-15 bg-[#f5fffa]">
             <div className="flex flex-row justify-between items-center mt-4 mb-4">
               <input
                 type="text"
