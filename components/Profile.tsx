caa833bdf0788cf03e606a610c9ee2d772652cf3
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-05 18:21:59 +0530

    Add Profile component with card layout

---
+++ components/Profile.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card } from "./ui/card";
+
+const Notice = () => {
+  return (
+    <Card className="bg-[#f5fffa] w-[1028px] h-[65px] flex items-center p-4">
+      <div className="flex items-center gap-4">
+      </div>
+    </Card>
+  );
+};
+
+export default Notice;
