5e8da79322207166537c865c9bb9e56bafd728ef
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-06 10:13:02 +0530

    Add LicenseInfo component with initial layout

---
+++ components/LiscenseInfo.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const LicenseInfo = () => {
+  return (
+    <div className="w-[332px] h-[235px] bg-[#f5fffa] rounded-lg p-4 border"></div>
+  );
+};
+export default LicenseInfo;
