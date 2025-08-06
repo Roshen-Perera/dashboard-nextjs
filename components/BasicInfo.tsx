923a512f2f0eb8314fc3f0401ad78a1ffc0fa73c
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-07 01:44:44 +0530

    Refactor BasicInfo component: enhance structure by adding content within Card component

--- components/BasicInfo.tsx
+++ components/BasicInfo.tsx
@@ -3,7 +3,25 @@ import { Card } from './ui/card';
 
 const BasicInfo = () => {
   return (
-    <Card className="w-[332px] h-[235px] bg-[#f5fffa]"></Card>
+    <Card className="w-[332px] h-[235px] bg-[#f5fffa] p-4">
+      <div className="ml-3 flex flex-col">
+        <p className="text-xl mb-3">Basic Information</p>
+        <div className='flex flex-col gap-2'>
+          <div className="flex flex-col">
+            <p className="text-base text-gray-500">Department</p>
+            <p className="text-base">Cardiology</p>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-base text-gray-500">Qualifications</p>
+            <p className="text-base">MBBS, MD</p>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-base text-gray-500">Languages</p>
+            <p className="text-base">English, Spanish</p>
+          </div>
+        </div>
+      </div>
+    </Card>
   );
 };
 export default BasicInfo;
