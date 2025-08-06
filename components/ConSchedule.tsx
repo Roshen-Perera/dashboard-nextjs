a4e85033e678a89b88fc4f566313a9424aa11d21
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-07 02:05:59 +0530

    Refactor ConSchedule component: enhance structure by adding detailed schedule information within Card component

--- components/ConSchedule.tsx
+++ components/ConSchedule.tsx
@@ -3,7 +3,58 @@ import { Card } from './ui/card';
 
 const ConSchedule = () => {
   return (
-    <Card className="w-[558px] h-[180px] bg-[#f5fffa] rounded-lg p-4 border"></Card>
+    <Card className="w-[558px] h-[180px] bg-[#f5fffa] p-4">
+      <div className="ml-2">
+        <p className="text-xl mb-3">Basic Information</p>
+        <div className="flex flex-row justify-around">
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-xs">Mon</p>
+            <div className="bg-[#0099665b] p-2 rounded-md">
+              <p className="text-xs">9:00 - 12:00</p>
+            </div>
+            <div className="bg-[#0099665b] p-2 rounded-md">
+              <p className="text-xs">9:00 - 12:00</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-xs">Mon</p>
+            <div className="bg-[#0099665b] p-2 rounded-md">
+              <p className="text-xs">9:00 - 12:00</p>
+            </div>
+            <div className="bg-[#0099665b] p-2 rounded-md">
+              <p className="text-xs">9:00 - 12:00</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-xs">Tue</p>
+            <div className="bg-[#0099665b] p-2 rounded-md">
+              <p className="text-xs">9:00 - 12:00</p>
+            </div>
+            <div className="bg-[#0099665b] p-2 rounded-md">
+              <p className="text-xs">9:00 - 12:00</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-xs">Thu</p>
+            <div className="bg-[#0099665b] p-2 rounded-md">
+              <p className="text-xs">9:00 - 12:00</p>
+            </div>
+            <div className="bg-[#0099665b] p-2 rounded-md">
+              <p className="text-xs">9:00 - 12:00</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-xs">Fri</p>
+            <div className="bg-[#0099665b] p-2 rounded-md">
+              <p className="text-xs">9:00 - 12:00</p>
+            </div>
+            <div className="bg-[#0099665b] p-2 rounded-md">
+              <p className="text-xs">9:00 - 12:00</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Card>
   );
 };
 
