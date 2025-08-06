09332e46d2da94e08835d71e9cec55a113be1089
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-06 12:21:38 +0530

    Add ProTimeline component with initial structure

---
+++ components/ProTimeline.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProTimeline = () => {
+  return (
+    <div className="w-[328px] h-[649px] bg-[#f5fffa] flex items-center">
+        
+    </div>
+  );
+}
+
+export default ProTimeline
