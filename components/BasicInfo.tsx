da1e7e60fd06d4b8d1782ddc141f91672805849e
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-06 10:01:54 +0530

    Add BasicInfo component with initial layout

---
+++ components/BasicInfo.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const BasicInfo = () => {
+  return (
+    <div className="w-[332px] h-[235px] bg-[#f5fffa] rounded-lg p-4 shadow-md"></div>
+  );
+}
+export default BasicInfo
