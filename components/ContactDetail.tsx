46c6e7af1a11119605dfe7291d9a1f2968226d43
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-07 01:49:03 +0530

    Refactor ContactDetails component: enhance structure by adding content within Card component

--- components/ContactDetail.tsx
+++ components/ContactDetail.tsx
@@ -3,7 +3,25 @@ import { Card } from './ui/card';
 
 const ContactDetails = () => {
   return (
-    <Card className="w-[332px] h-[235px] bg-[#f5fffa]"></Card>
+    <Card className="w-[332px] h-[235px] bg-[#f5fffa] p-4">
+      <div className="ml-3 flex flex-col">
+        <p className="text-xl mb-3">Contact Details</p>
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col">
+            <p className="text-base text-gray-500">Email</p>
+            <p className="text-base">example@gmail.com</p>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-base text-gray-500">Phone Number</p>
+            <p className="text-base">+94-75-578-3011</p>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-base text-gray-500">Office Phone</p>
+            <p className="text-base">+94-75-578-3011</p>
+          </div>
+        </div>
+      </div>
+    </Card>
   );
 };
 export default ContactDetails;
