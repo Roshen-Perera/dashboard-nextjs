731c646f14ed668faeaedaf0bcd560ded0289d73
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-07 18:40:11 +0530

    Refactor SpecialtyAreas component: improve code structure and formatting

--- components/SpecialtyAreas.tsx
+++ components/SpecialtyAreas.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
+
+import React from "react";
 
 const SpecialtyAreas = () => {
+  const SpecialtyAreas = [
+    "Heart failure",
+    "Preventive Cardiology",
+    "Echocardiography",
+    "Cardiac Imaging",
+    "Electrophysiology",
+  ];
   return (
     <div className="w-[558px] h-[180px] bg-[#f5fffa] pt-4">
       <div>
         <p className="text-xl mb-3">Specialty Areas</p>
-        
+        <div className="flex flex-wrap gap-3">
+          {Object.entries(SpecialtyAreas).map(([key, value]) => (
+            <div className="bg-[#0099665b] p-1.5 rounded-xl" key={key}>
+              <p className="text-xs">{value}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default SpecialtyAreas
+export default SpecialtyAreas;
