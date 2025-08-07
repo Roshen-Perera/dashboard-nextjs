3a030ba0fa5290ecf8023aeb82e7c8273d4c0fc8
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-07 18:18:03 +0530

    Add initial structure for SpecialtyAreas component

--- components/SpecialtyAreas.tsx
+++ components/SpecialtyAreas.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const SpecialtyAreas = () => {
   return (
-    <div className=''>
-            
+    <div className="w-[558px] h-[180px] bg-[#f5fffa]">
+      <div className="ml-2">
+        <p className="text-xl mb-3">Specialty Areas</p>
+        
+      </div>
     </div>
-  )
+  );
 }
 
 export default SpecialtyAreas
