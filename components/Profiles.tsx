dd5a4aa8329c3b2a38f2858496835532fa035251
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-07 10:58:34 +0530

    Refactor Profile component: enhance layout structure and ensure consistent formatting

--- components/Profiles.tsx
+++ components/Profiles.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Card } from "./ui/card";
+import Image from "next/image";
 
 const Profile = () => {
   return (
-    <Card className="bg-[#f5fffa] w-[1028px] h-[245px] flex items-center  p-4">
-
+    <Card className="bg-[#f5fffa] w-[1028px] h-[245px]  p-4">
+      <div>
+        <Image
+          src={"/assets/sarah.png"}
+          width={200}
+          height={200}
+          alt="Doctor Profile"
+          className="rounded-full"
+        />
+      </div>
     </Card>
   );
 };
 
 export default Profile;
-  
