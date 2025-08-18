377e8809402a36eb836986b31f3088f969e4865f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-18 14:20:38 +0530

    Refactor TodayAppointmentCount component: improve layout and spacing

--- components/TodayAppointmentCount.tsx
+++ components/TodayAppointmentCount.tsx
@@ -4,11 +4,14 @@ import { Calendar } from 'lucide-react';
 
 const TodayAppointmentCount = () => {
   return (
-    <Card className="bg-[#f5fffa] w-[250px] h-[100px] p-6 rounded-md">
-      <div className='flex flex-row'>
-        <Calendar />
-        <div className='flex flex-col'>
-          
+    <Card className="bg-[#f5fffa] w-[250px] h-[100px] p-5 rounded-md">
+      <div className="flex flex-row gap-4 items-center">
+        <div>
+          <Calendar />
+        </div>
+        <div className="flex flex-col">
+          <p className="text-lg text-gray-500">Today</p>
+          <p className="text-xl text-gray-900">12</p>
         </div>
       </div>
     </Card>
