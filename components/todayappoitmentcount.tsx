72b2752e2812c0d738d8890282790a24496b8bfb
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-18 13:33:30 +0530

    Add Home component with sidebar and search functionality; create today appointment count component

---
+++ components/todayappoitmentcount.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card } from './ui/card';
+
+const todayappoitmentcount = () => {
+  return (
+    <Card className="bg-[#f5fffa] w-[245px] h-[100px] p-6">
+        
+    </Card>
+  );
+}
+
+export default todayappoitmentcount
