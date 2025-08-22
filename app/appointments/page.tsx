4ed7283769684b2b9b8018cef965199f5123c972
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-22 13:52:37 +0530

    Removed header

--- app/appointments/page.tsx
+++ app/appointments/page.tsx
@@ -16,35 +16,7 @@ const Home = () => {
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset className="bg-[#f5fffa]">
-          <div className="pl-4 pr-8">
-            <div className="flex flex-row justify-between items-center mt-4 mb-4">
-              <input
-                type="text"
-                placeholder="Search..."
-                className="text-sm px-3 py-2 border border-gray-300 rounded-3xl focus:outline-none focus:ring-2 focus:ring-green-400"
-              />
-              <div className="flex items-center gap-2 text-sm font-medium rounded-lg pr-1 pl-1">
-                <Image
-                  src={"/assets/icons/bell.png"}
-                  width={20}
-                  height={20}
-                  alt="Notification"
-                />
-                |
-                <Image
-                  src={"/assets/dragisa.png"}
-                  width={30}
-                  height={30}
-                  alt="Profile"
-                  className="rounded-full"
-                />
-                <div>
-                  <p className="text-sm">Dr. Dragisa</p>
-                  <p className="text-xs text-gray-500">Jun 25, 2024 06:00 AM</p>
-                </div>
-              </div>
-            </div>
-          </div>
+          
           <div className="flex justify-between p-5">
             <TodayAppointmentCount />
             <TodayUpcomingCount />
