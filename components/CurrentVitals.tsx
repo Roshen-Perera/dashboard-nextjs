436157e0d395c887230d65bfc9e1bb30d0f22a11
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-26 18:09:59 +0530

    Refactor CurrentVitals component: remove duplicate text and improve structure for clarity

--- components/CurrentVitals.tsx
+++ components/CurrentVitals.tsx
@@ -1,14 +1,19 @@
+import { Edit03Icon } from '@hugeicons/core-free-icons';
+import { HugeiconsIcon } from '@hugeicons/react';
 import React from 'react'
 
 const CurrentVitals = () => {
   return (
     <div>
-      <div className='flex flex-row justify-around'>
-        <div className='text-base'>Current Vitals</div>
-        <div className='text-base'>Current Vitals</div>
+      <div className="flex flex-row justify-around">
+        <div className="text-base">Current Vitals</div>
+        <div className="flex items-center gap-2">
+          <HugeiconsIcon icon={Edit03Icon} className="w-4 h-4 text-green-500" />
+          <p className="text-sm text-green-500">Edit</p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default CurrentVitals
