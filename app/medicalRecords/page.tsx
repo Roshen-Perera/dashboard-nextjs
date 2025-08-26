598413de6ee85ea427dea88cae697905a9e635fa
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-26 15:56:13 +0530

    Enhance layout and styling in Home component for improved user experience

--- app/medicalRecords/page.tsx
+++ app/medicalRecords/page.tsx
@@ -5,6 +5,7 @@ import PageHeader from "@/components/PageHeader";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import React from "react";
 import Image from "next/image";
+import { Download } from "lucide-react";
 
 const Home = () => {
   return (
@@ -14,7 +15,7 @@ const Home = () => {
         <SidebarInset className="bg-[#f5fffa]">
           <div className="pl-4 pr-8">
             <PageHeader />
-            <div className="flex flex-row pt-4">
+            <div className="flex flex-row pt-4 justify-between">
               <div className="flex flex-row gap-4">
                 <div>
                   <Image
@@ -26,19 +27,28 @@ const Home = () => {
                   />
                 </div>
                 <div className="mt-3">
-                  <h1 className="text-2xl text-gray-900 mb-1">
+                  <h1 className="text-3xl text-gray-900 mb-1">
                     Dr. Sarah Johnson
                   </h1>
                   <div className="flex flex-row gap-4">
-                    <p className="text-base text-gray-500">Female, 35 years</p>
-                    <p className="text-base text-gray-500">ID 20041231030</p>
-                    <p className="text-base text-gray-500">
+                    <p className="text-lg text-gray-500">Female, 35 years</p>
+                    <p className="text-lg text-gray-500">ID 20041231030</p>
+                    <p className="text-lg text-gray-500">
                       Phone: (+94) 75 578 3011
                     </p>
                   </div>
                 </div>
               </div>
-              <div></div>
+              <div className="flex flex-row gap-x-4 items-center">
+                <button className="flex flex-row items-center gap-2 px-4 py-2 bg-white text-black text-sm hover:bg-gray-200 border-2 border-black rounded-sm transition-colors w-fit h-fit">
+                  <Download className="w-4 h-4" />
+                  Export
+                </button>
+                <button className="flex flex-row items-center gap-2 px-4 py-2 bg-green-600 text-white text-sm border-green-600 border-2 hover:border-green-900 hover:bg-green-900 rounded-sm transition-colors w-fit h-fit">
+                  <Download className="w-4 h-4" />
+                  Export
+                </button>
+              </div>
             </div>
           </div>
         </SidebarInset>
