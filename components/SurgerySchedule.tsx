d916cdd00d9d7f1c22140706e7ea229d93f4e82e
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-26 22:09:20 +0530

    Enhance SurgerySchedule component with padding for improved layout

--- components/SurgerySchedule.tsx
+++ components/SurgerySchedule.tsx
@@ -1,13 +1,20 @@
+import { Plus } from 'lucide-react';
 import React from 'react'
 
 const SurgerySchedule = () => {
   return (
     <div>
-      <div className='w-[1060px] h-[330px] border-1 rounded-sm'>
-
+      <div className="w-[1060px] h-[330px] border-1 rounded-sm p-2">
+        <div className="flex flex-row justify-between p-2">
+          <div className="text-xl">Surgeries</div>
+          <div className="flex items-center gap-2">
+            <Plus/>
+            <p className="text-base text-green-500">Schedule Surgery</p>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default SurgerySchedule
