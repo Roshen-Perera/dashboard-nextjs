690f542d0dcf71e11d761ebd74668c2d7527b5ae
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-26 14:58:47 +0530

    Refactor Home component to improve code consistency and formatting

--- app/medicalRecords/page.tsx
+++ app/medicalRecords/page.tsx
@@ -1,9 +1,10 @@
-"use client"
+"use client";
 
-import { AppSidebar } from '@/components/app-sidebar';
-import PageHeader from '@/components/PageHeader';
-import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
-import React from 'react'
+import { AppSidebar } from "@/components/app-sidebar";
+import PageHeader from "@/components/PageHeader";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import React from "react";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -13,11 +14,26 @@ const Home = () => {
         <SidebarInset className="bg-[#f5fffa]">
           <div className="pl-4 pr-8">
             <PageHeader />
+            <div className="flex flex-row">
+              <div>
+                <h1 className="text-2xl text-gray-900 mb-1">
+                  Dr. Sarah Johnson
+                </h1>
+                <div className="flex flex-row gap-4">
+                  <p className="text-base text-gray-500">Female, 35 years</p>
+                  <p className="text-base text-gray-500">ID 20041231030</p>
+                  <p className="text-base text-gray-500">
+                    Phone: (+94) 75 578 3011
+                  </p>
+                </div>
+              </div>
+              <div></div>
+            </div>
           </div>
         </SidebarInset>
       </SidebarProvider>
     </>
   );
-}
+};
 
 export default Home;
