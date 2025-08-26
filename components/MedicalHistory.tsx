8f370d66476e6fd357914dc179b1af10102b3b43
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-26 22:01:06 +0530

    Refactor MedicalHistory layout and enhance SurgerySchedule styling

--- components/MedicalHistory.tsx
+++ components/MedicalHistory.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import CurrentVitals from './CurrentVitals'
 import Biometrics from './Biometrics'
+import SurgerySchedule from './SurgerySchedule'
 
 const MedicalHistory = () => {
   return (
-    <div className='flex flex-row justify-between'>
-      <CurrentVitals/>
-      <Biometrics/>
-    </div>
-  )
+    <>
+      <div>
+        <div className="flex flex-row justify-between">
+          <CurrentVitals />
+          <Biometrics />
+        </div>
+        <div className='mt-4'>
+          <SurgerySchedule />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default MedicalHistory
--- components/SurgerySchedule.tsx
+++ components/SurgerySchedule.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SurgerySchedule = () => {
   return (
     <div>
-      <div className='w-[1060px] h-[330px] border-1'>
+      <div className='w-[1060px] h-[330px] border-1 rounded-sm'>
 
       </div>
     </div>
