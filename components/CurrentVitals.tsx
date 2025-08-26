49e28d8a68422b3e0216c6f127d3d04ccd1e3a61
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-26 22:11:41 +0530

    Improve styling in CurrentVitals component for better readability

--- components/CurrentVitals.tsx
+++ components/CurrentVitals.tsx
@@ -5,9 +5,9 @@ import React from "react";
 const CurrentVitals = () => {
   return (
     <div>
-      <div className="w-[520px] h-[252px] border-1 rounded-sm p-2">
+      <div className="w-[520px] h-[252px] border-1 rounded-sm p-1">
         <div className="flex flex-row justify-between p-2">
-          <div className="text-base">Current Vitals</div>
+          <div className="text-xl">Current Vitals</div>
           <div className="flex items-center gap-2">
             <HugeiconsIcon
               icon={Edit03Icon}
@@ -16,26 +16,26 @@ const CurrentVitals = () => {
             <p className="text-sm text-green-500">Edit</p>
           </div>
         </div>
-        <div className="mt-2">
+        <div>
           <div className="flex flex-row justify-between p-2">
-            <div className="text-sm text-gray-500">Blood Pressure</div>
-            <div className="text-sm">120/80 mmHg</div>
+            <div className="text-base text-gray-500">Blood Pressure</div>
+            <div className="text-base">120/80 mmHg</div>
           </div>
           <div className="flex flex-row justify-between p-2">
-            <div className="text-sm text-gray-500">Heart Rate</div>
-            <div className="text-sm">72 bpm</div>
+            <div className="text-base text-gray-500">Heart Rate</div>
+            <div className="text-base">72 bpm</div>
           </div>
           <div className="flex flex-row justify-between p-2">
-            <div className="text-sm text-gray-500">Temperature</div>
-            <div className="text-sm">98.6°F</div>
+            <div className="text-base text-gray-500">Temperature</div>
+            <div className="text-base">98.6°F</div>
           </div>
           <div className="flex flex-row justify-between p-2">
-            <div className="text-sm text-gray-500">Oxygen Saturation</div>
-            <div className="text-sm">98%</div>
+            <div className="text-base text-gray-500">Oxygen Saturation</div>
+            <div className="text-base">98%</div>
           </div>
           <div className="flex flex-row justify-between p-2">
-            <div className="text-sm text-gray-500">Blood Sugar</div>
-            <div className="text-sm">20mg/dL</div>
+            <div className="text-base text-gray-500">Blood Sugar</div>
+            <div className="text-base">20mg/dL</div>
           </div>
         </div>
       </div>
