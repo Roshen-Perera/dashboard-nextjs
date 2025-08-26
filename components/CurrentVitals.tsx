53c49ae0532278afa926a3ca6e2d3ccc91fd35f4
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-26 18:07:31 +0530

    Add CurrentVitals and MedicalHistory components with initial structure

---
+++ components/CurrentVitals.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CurrentVitals = () => {
+  return (
+    <div>
+      <div className='flex flex-row justify-around'>
+        <div className='text-base'>Current Vitals</div>
+        <div className='text-base'>Current Vitals</div>
+      </div>
+    </div>
+  )
+}
+
+export default CurrentVitals
