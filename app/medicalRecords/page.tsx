a9a305c5bc77d996374fad8f25ca15ad5ed77467
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-26 17:06:52 +0530

    Refactor Home component: adjust image size, update button labels, and improve styling for consistency

--- app/medicalRecords/page.tsx
+++ app/medicalRecords/page.tsx
@@ -5,7 +5,7 @@ import PageHeader from "@/components/PageHeader";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import React from "react";
 import Image from "next/image";
-import { Download } from "lucide-react";
+import { Download, Printer } from "lucide-react";
 
 const Home = () => {
   return (
@@ -20,13 +20,13 @@ const Home = () => {
                 <div>
                   <Image
                     src={"/assets/sarah.png"}
-                    width={100}
-                    height={100}
+                    width={80}
+                    height={80}
                     alt="Doctor Profile"
                     className="rounded-full"
                   />
                 </div>
-                <div className="mt-3">
+                <div className="mt-1">
                   <h1 className="text-3xl text-gray-900 mb-1">
                     Dr. Sarah Johnson
                   </h1>
@@ -40,16 +40,17 @@ const Home = () => {
                 </div>
               </div>
               <div className="flex flex-row gap-x-4 items-center">
-                <button className="flex flex-row items-center gap-2 px-4 py-2 bg-[#f5fffa] text-black text-sm hover:bg-gray-200 border-2 border-black rounded-sm transition-colors w-fit h-fit">
-                  <Download className="w-4 h-4" />
-                  Export
+                <button className="flex flex-row items-center gap-2 px-4 py-2 bg-[#f5fffa] text-black text-base hover:bg-gray-200 border-2 border-black rounded-sm transition-colors w-fit h-fit">
+                  <Printer className="w-4 h-4" />
+                  Print
                 </button>
-                <button className="flex flex-row items-center gap-2 px-4 py-2 bg-green-600 text-white text-sm border-green-600 border-2 hover:border-green-900 hover:bg-green-900 rounded-sm transition-colors w-fit h-fit">
+                <button className="flex flex-row items-center gap-2 px-4 py-2 bg-green-600 text-white text-base border-green-600 border-2 hover:border-green-900 hover:bg-green-900 rounded-sm transition-colors w-fit h-fit">
                   <Download className="w-4 h-4" />
                   Export
                 </button>
               </div>
             </div>
+            
           </div>
         </SidebarInset>
       </SidebarProvider>
