dbadc5dba552980d9d83c8241c212a8ee6349ad7
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-27 14:15:00 +0530

    Add initial structure to Medications component

--- components/Medications.tsx
+++ components/Medications.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Medications = () => {
   return (
     <div>
-      
+       <div className="w-[1060px] flex flex-row justify-around border-1 rounded-sm p-2 items-center">
+            <div className='w-[240px] h-[80px]'></div>
+            <div className='w-[240px] h-[80px]'></div>
+            <div className='w-[240px] h-[80px]'></div>
+            <div className='w-[240px] h-[80px]'></div>
+       </div>
     </div>
   )
 }
