ebdfc72c375e4d80d9597602433c3a9cee188fff
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-27 18:02:15 +0530

    Refactor Medications component layout for improved structure and readability

--- components/Medications.tsx
+++ components/Medications.tsx
@@ -1,16 +1,78 @@
-import React from 'react'
+import { Plus } from "lucide-react";
+import React from "react";
 
 const Medications = () => {
   return (
     <div>
-      <div className="w-[1060px] flex flex-row justify-around border-1 rounded-sm p-2 items-center">
-        <div className="w-[240px] h-[80px] border-1"></div>
-        <div className="w-[240px] h-[80px] border-1"></div>
-        <div className="w-[240px] h-[80px] border-1"></div>
-        <div className="w-[240px] h-[80px] border-1"></div>
+      <div className="w-[1060px] border-1 rounded-sm p-2 items-center">
+        <div className="flex flex-row justify-between p-2">
+          <div className="text-xl">Medications</div>
+          <div className="flex items-center text-green-500 gap-2">
+            <p className="text-base">New Medication</p>
+            <Plus />
+          </div>
+        </div>
+        <div className="flex flex-row justify-around">
+          <div className="w-[240px] border-1 rounded-sm">
+            <div className="flex flex-row justify-between p-2">
+              <div className="">
+                <p className="text-xl -mt-1 font-normal text-gray-800">Propranolol</p>
+                <p className="text-sm mt-2 font-normal text-gray-500">500mg</p>
+                <p className="text-sm mt-2 font-normal text-gray-500">2x daily (day, night)</p>
+              </div>
+              <div>
+                <span className="bg-[#0099665b] px-3 py-1 rounded-xl text-xs font-normal">
+                  Finished
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="w-[240px] border-1 rounded-sm">
+            <div className="flex flex-row justify-between p-2">
+              <div className="">
+                <p className="text-xl -mt-1 font-normal text-gray-800">Propranolol</p>
+                <p className="text-sm mt-2 font-normal text-gray-500">500mg</p>
+                <p className="text-sm mt-2 font-normal text-gray-500">2x daily (day, night)</p>
+              </div>
+              <div>
+                <span className="bg-[#0099665b] px-3 py-1 rounded-xl text-xs font-normal">
+                  Finished
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="w-[240px] border-1 rounded-sm">
+            <div className="flex flex-row justify-between p-2">
+              <div className="">
+                <p className="text-xl -mt-1 font-normal text-gray-800">Propranolol</p>
+                <p className="text-sm mt-2 font-normal text-gray-500">500mg</p>
+                <p className="text-sm mt-2 font-normal text-gray-500">2x daily (day, night)</p>
+              </div>
+              <div>
+                <span className="bg-[#0099665b] px-3 py-1 rounded-xl text-xs font-normal">
+                  Finished
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="w-[240px] border-1 rounded-sm">
+            <div className="flex flex-row justify-between p-2">
+              <div className="">
+                <p className="text-xl -mt-1 font-normal text-gray-800">Propranolol</p>
+                <p className="text-sm mt-2 font-normal text-gray-500">500mg</p>
+                <p className="text-sm mt-2 font-normal text-gray-500">2x daily (day, night)</p>
+              </div>
+              <div>
+                <span className="bg-[#0099665b] px-3 py-1 rounded-xl text-xs font-normal">
+                  Finished
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Medications
+export default Medications;
