27e183b5eb6beb1959035e7a12be61a65594788f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-28 15:30:40 +0530

    Refactor AdmissionHistory component to use state for histories and improve layout with status display

--- components/AdmissionHistory.tsx
+++ components/AdmissionHistory.tsx
@@ -1,8 +1,29 @@
 import { Plus } from "lucide-react";
-import React from "react";
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./ui/accordion";
+import React, { useState } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./ui/accordion";
 
 const AdmissionHistory = () => {
+    const [histories] = useState([
+      {
+        id: 1,
+        department: "Cardiology",
+        period: "May 10, 2025 - May 14, 2025",
+        status: "Discharged",
+        doctor: "Dr Lorem Ipsum",
+      },
+      {
+        id: 2,
+        department: "Internal Medicine",
+        period: "May 10, 2025",
+        status: "Active",
+        doctor: "Dr Lorem Ipsum",
+      },
+    ]);
   return (
     <div>
       <div className="w-[1060px] border-1 rounded-sm p-2 items-center">
@@ -13,48 +34,55 @@ const AdmissionHistory = () => {
             <Plus />
           </div>
         </div>
-        <div className="grid grid-cols-4 bg-gray-200 text-lg font-normal p-4 rounded-t-sm">
+        <div className="grid grid-cols-4 mt-3 bg-gray-200 text-lg font-normal p-4 rounded-t-sm">
           <div className="whitespace-nowrap">Department</div>
           <div className="whitespace-nowrap -ml-4">Admission Period</div>
-          <div className="whitespace-nowrap ml-6">Status</div>
+          <div className="whitespace-nowrap ml-8">Status</div>
           <div className="whitespace-nowrap">Attending Doctor</div>
         </div>
+        {histories.map((history) => (
+          <Accordion key={history.id} type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger className="flex flex-row items-center text-base p-4 -mb-2 font-normal">
+                <div className="whitespace-nowrap">{history.department}</div>
+                <div className="whitespace-nowrap">
+                  {history.period}
+                </div>
+                <div>
+                  <span className="bg-[#0099665b] px-3 py-1 rounded-xl text-xs font-normal">
+                    {history.status}
+                  </span>
+                </div>
+                <div className="whitespace-nowrap">{history.doctor}</div>
+              </AccordionTrigger>
 
-        <Accordion type="single" collapsible>
-          <AccordionItem value="item-1">
-            <AccordionTrigger className="flex flex-row items-center text-base p-4 -mb-2 font-normal">
-              <div className="whitespace-nowrap">John Doe</div>
-              <div className="whitespace-nowrap">27 Aug, 10:30 AM</div>
-              <div className="whitespace-nowrap">Checkup</div>
-              <div className="whitespace-nowrap">Approved</div>
-            </AccordionTrigger>
+              <AccordionContent className="p-4">
+                <div className="mx-auto border-1 rounded-sm -mt-4 p-4">
+                  <div className="0 rounded">
+                    <div className="flex">
+                      <div className="w-32">
+                        <button
+                          className={
+                            "w-full px-3 py-2 text-left text-sm last:border-b-0 font-semibold border-l-2 border-l-green-500"
+                          }
+                        >
+                          Notes
+                        </button>
+                      </div>
 
-            <AccordionContent className="p-4">
-              <div className="mx-auto border-1 rounded-sm -mt-4 p-4">
-                <div className="0 rounded">
-                  <div className="flex">
-                    <div className="w-32">
-                      <button
-                        className={
-                          "w-full px-3 py-2 text-left text-sm last:border-b-0 font-semibold border-l-2 border-l-green-500"
-                        }
-                      >
-                        Notes
-                      </button>
-                    </div>
-
-                    <div className="flex-1 min-h-40">
-                      <textarea
-                        className="w-full h-full min-h-40 p-3 border rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                        placeholder={`Enter Notes here...`}
-                      />
+                      <div className="flex-1 min-h-40">
+                        <textarea
+                          className="w-full h-full min-h-40 p-3 border rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                          placeholder={`Enter Notes here...`}
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        ))}
       </div>
     </div>
   );
