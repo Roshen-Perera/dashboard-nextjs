60f1d8415077f2d4adc7fbad037906cf9738314d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-28 15:19:57 +0530

    Enhance AdmissionHistory component with accordion for detailed notes and improved layout

--- components/AdmissionHistory.tsx
+++ components/AdmissionHistory.tsx
@@ -1,5 +1,6 @@
 import { Plus } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./ui/accordion";
 
 const AdmissionHistory = () => {
   return (
@@ -12,6 +13,48 @@ const AdmissionHistory = () => {
             <Plus />
           </div>
         </div>
+        <div className="grid grid-cols-4 bg-gray-200 text-lg font-normal p-4 rounded-t-sm">
+          <div className="whitespace-nowrap">Department</div>
+          <div className="whitespace-nowrap -ml-4">Admission Period</div>
+          <div className="whitespace-nowrap ml-6">Status</div>
+          <div className="whitespace-nowrap">Attending Doctor</div>
+        </div>
+
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger className="flex flex-row items-center text-base p-4 -mb-2 font-normal">
+              <div className="whitespace-nowrap">John Doe</div>
+              <div className="whitespace-nowrap">27 Aug, 10:30 AM</div>
+              <div className="whitespace-nowrap">Checkup</div>
+              <div className="whitespace-nowrap">Approved</div>
+            </AccordionTrigger>
+
+            <AccordionContent className="p-4">
+              <div className="mx-auto border-1 rounded-sm -mt-4 p-4">
+                <div className="0 rounded">
+                  <div className="flex">
+                    <div className="w-32">
+                      <button
+                        className={
+                          "w-full px-3 py-2 text-left text-sm last:border-b-0 font-semibold border-l-2 border-l-green-500"
+                        }
+                      >
+                        Notes
+                      </button>
+                    </div>
+
+                    <div className="flex-1 min-h-40">
+                      <textarea
+                        className="w-full h-full min-h-40 p-3 border rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                        placeholder={`Enter Notes here...`}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </div>
     </div>
   );
