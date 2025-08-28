491717c5a8f5c424014c196a4bcbd907f37107a0
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-28 13:29:22 +0530

    Refactor AdmissionHistory component structure for improved layout and readability

--- components/AdmissionHistory.tsx
+++ components/AdmissionHistory.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { Plus } from "lucide-react";
+import React from "react";
 
 const AdmissionHistory = () => {
   return (
     <div>
-      
+      <div className="w-[1060px] border-1 rounded-sm p-2 items-center">
+        <div className="flex flex-row justify-between p-2">
+          <div className="text-xl">Surgeries</div>
+          <div className="flex items-center text-green-500 gap-2">
+            <p className="text-base">Schedule Surgery</p>
+            <Plus />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdmissionHistory
+export default AdmissionHistory;
