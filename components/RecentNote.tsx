b4c15b5f78dc4ebd42dc29eba560b15cbab03782
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-28 11:22:24 +0530

    Refactor RecentNote component to adjust margin styles for improved layout

--- components/RecentNote.tsx
+++ components/RecentNote.tsx
@@ -4,8 +4,8 @@ const RecentNote = () => {
   return (
     <div>
       <div className="p-2">
-        <p className="text-sm mt-1 font-normal text-gray-500">Feb 20, 2025 </p>
-        <p className="text-base mt-1 font-normal text-gray-800">
+        <p className="text-sm font-normal text-gray-500">Feb 20, 2025 </p>
+        <p className="text-base font-normal text-gray-800">
           Lorem ipsum ipsum viverra sed convallis ut vitae augue nulla pharetra
           cum in a odio.{" "}
         </p>
