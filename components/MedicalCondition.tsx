e9bd7f9bfd5f4b6cf06cbb9b843eaa80d71d4add
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-28 10:42:36 +0530

    Implement MedicalCondition component with initial layout and data

--- components/MedicalCondition.tsx
+++ components/MedicalCondition.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
 const MedicalCondition = () => {
-  return <div></div>;
-};
+  return (
+    <div>
+      <div className="flex flex-row justify-between p-2">
+        <div className="">
+          <p className="text-xl -mt-1 font-normal text-gray-800">
+            Type 2 Diabetes
+          </p>
+          <p className="text-sm mt-2 font-normal text-gray-500">
+            Diagnosed: jan 2020
+          </p>
+        </div>
+        <div>
+          <span className="bg-[#0099665b] px-3 py-1 rounded-xl text-xs font-normal">
+            Monitored
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-export default MedicalCondition;
+export default MedicalCondition
