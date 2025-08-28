cbb04c999db647670a499ef1e1abc7171811e314
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-28 10:37:12 +0530

    Refactor MedicalConditions component layout for improved structure

--- components/MedicalConditions.tsx
+++ components/MedicalConditions.tsx
@@ -1,13 +1,20 @@
+import { Plus } from 'lucide-react';
 import React from 'react'
 
 const MedicalConditions = () => {
   return (
     <div>
       <div className="w-[520px] h-[285px] border-1 rounded-sm p-1">
-
+        <div className="flex flex-row justify-between p-2">
+          <div className="text-xl">Medical Conditions</div>
+          <div className="flex items-center text-green-500 gap-2">
+            <p className="text-base">Add New</p>
+            <Plus />
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default MedicalConditions
