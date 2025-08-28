c2a3805f5f0c2b36a67d83cde7cc90154058a970
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-28 11:51:04 +0530

    Add VisitHistory component for displaying user visit history

---
+++ components/VisitHistory.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const VisitHistory = () => {
+  return (
+    <div className="w-[1060px] border-1 rounded-sm p-2 items-center">
+      <div className="flex flex-row justify-between p-2">
+        <div className="text-xl">Visit History</div>
+      </div>
+    </div>
+  );
+};
+
+export default VisitHistory;
