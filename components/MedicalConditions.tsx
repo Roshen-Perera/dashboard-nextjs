4fd843504fae5ea48499d913f462ce3f4f4d716f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-28 10:38:13 +0530

    Increase padding in MedicalConditions component for better spacing

--- components/MedicalConditions.tsx
+++ components/MedicalConditions.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const MedicalConditions = () => {
   return (
     <div>
-      <div className="w-[520px] h-[285px] border-1 rounded-sm p-1">
+      <div className="w-[520px] h-[285px] border-1 rounded-sm p-2">
         <div className="flex flex-row justify-between p-2">
           <div className="text-xl">Medical Conditions</div>
           <div className="flex items-center text-green-500 gap-2">
