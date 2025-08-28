01c95b9a8597aff45e9b1b0f14babaf0662462b8
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-28 10:19:21 +0530

    Add RecentNotes component for displaying recent patient notes

---
+++ components/RecentNotes.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RecentNotes = () => {
+  return (
+    <div>
+      <div className="w-[520px] h-[285px] border-1 rounded-sm p-1"></div>
+    </div>
+  );
+};
+
+export default RecentNotes;
