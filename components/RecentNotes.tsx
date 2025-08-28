bb878b819dd5bd004ff61e2e04c28962f15cd6ab
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-28 11:15:25 +0530

    Refactor RecentNotes component structure for improved readability

--- components/RecentNotes.tsx
+++ components/RecentNotes.tsx
@@ -3,7 +3,19 @@ import React from 'react'
 const RecentNotes = () => {
   return (
     <div>
-      <div className="w-[520px] h-[295px] border-1 rounded-sm p-1"></div>
+      <div className="w-[520px] h-[295px] border-1 rounded-sm p-1">
+        <div className="flex flex-col">
+          <div className='p-2'>
+            <p className="text-sm mt-1 font-normal text-gray-500">
+              Feb 20, 2025{" "}
+            </p>
+            <p className="text-base -mt-1 font-normal text-gray-800">
+              Lorem ipsum ipsum viverra sed convallis ut vitae augue nulla
+              pharetra cum in a odio.{" "}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
