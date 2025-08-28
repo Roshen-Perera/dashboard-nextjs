a8897471576735694e5120afa8262b71d2e9b69a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-28 11:19:50 +0530

    Refactor RecentNotes component layout for improved structure and readability

--- components/RecentNotes.tsx
+++ components/RecentNotes.tsx
@@ -1,19 +1,19 @@
+import { Plus } from 'lucide-react';
 import React from 'react'
 
 const RecentNotes = () => {
   return (
     <div>
       <div className="w-[520px] h-[295px] border-1 rounded-sm p-1">
-        <div className="flex flex-col">
-          <div className='p-2'>
-            <p className="text-sm mt-1 font-normal text-gray-500">
-              Feb 20, 2025{" "}
-            </p>
-            <p className="text-base mt-1 font-normal text-gray-800">
-              Lorem ipsum ipsum viverra sed convallis ut vitae augue nulla
-              pharetra cum in a odio.{" "}
-            </p>
+        <div className="flex flex-row justify-between p-2">
+          <div className="text-xl">Medications</div>
+          <div className="flex items-center text-green-500 gap-2">
+            <p className="text-base">New Medication</p>
+            <Plus />
           </div>
+        </div>
+        <div className="flex flex-col -mt-1">
+          
         </div>
       </div>
     </div>
