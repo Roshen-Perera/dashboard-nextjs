c7bd2d311c310b96eda78e3e9a95826e6bd0260b
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-28 13:30:41 +0530

    Update AdmissionHistory component title and button label for clarity

--- components/AdmissionHistory.tsx
+++ components/AdmissionHistory.tsx
@@ -6,9 +6,9 @@ const AdmissionHistory = () => {
     <div>
       <div className="w-[1060px] border-1 rounded-sm p-2 items-center">
         <div className="flex flex-row justify-between p-2">
-          <div className="text-xl">Surgeries</div>
+          <div className="text-xl">Admission History</div>
           <div className="flex items-center text-green-500 gap-2">
-            <p className="text-base">Schedule Surgery</p>
+            <p className="text-base">New Admission</p>
             <Plus />
           </div>
         </div>
