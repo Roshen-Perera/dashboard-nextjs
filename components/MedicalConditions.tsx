443bd269155e2338e1fe193815b9d573d1521e5f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-28 10:13:38 +0530

    Add structure to MedicalConditions component

--- components/MedicalConditions.tsx
+++ components/MedicalConditions.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const MedicalConditions = () => {
   return (
     <div>
-      
+      <div className="w-[520px] h-[285px] border-1 rounded-sm p-1">
+
+      </div>
     </div>
   )
 }
