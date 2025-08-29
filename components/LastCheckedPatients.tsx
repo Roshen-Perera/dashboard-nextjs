3869d4f12b3fb5eae2d9c8a6134a6334b394c8ab
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-29 10:58:19 +0530

    Refactor LastCheckedPatients component to include patient details grid

--- components/LastCheckedPatients.tsx
+++ components/LastCheckedPatients.tsx
@@ -3,9 +3,14 @@ import React from 'react'
 const LastCheckedPatients = () => {
   return (
     <div>
-        
+      <div className="grid grid-cols-4 bg-gray-200 text-lg font-normal p-4 rounded-t-sm">
+        <div className="whitespace-nowrap">Patient Name</div>
+        <div className="whitespace-nowrap -ml-4">Patient ID</div>
+        <div className="whitespace-nowrap ml-6">Last Visit</div>
+        <div className="whitespace-nowrap">Visit Reason</div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default LastCheckedPatients
