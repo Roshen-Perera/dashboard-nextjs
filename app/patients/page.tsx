cc8a612016513ea64585c243d6cb80f1f955d940
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-08-29 10:24:30 +0530

    Implement Home component layout with sidebar and page header

--- app/patients/page.tsx
+++ app/patients/page.tsx
@@ -1,11 +1,21 @@
+import { AppSidebar } from '@/components/app-sidebar';
+import PageHeader from '@/components/PageHeader';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset className="bg-[#f5fffa]">
+          <div className="pl-4 pr-8">
+            <PageHeader />
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </>
+  );
 }
 
 export default Home
